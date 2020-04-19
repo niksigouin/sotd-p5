bc7d9cbca281c81119ccc93240e9e274bdf3e6a8
Nikolas Sigouin <nik.sigouin@gmail.com>
2020-04-19 00:46:02 -0400

    Main server funtionality

--- server.js
+++ server.js
@@ -1,27 +1,55 @@
+// BASIC INFO FOR PLAYER
+function Survivor(id_, name_, x_, y_, size_) {
+    this.id = id_;
+    this.name = name_;
+    this.x = x_;
+    this.y = y_;
+    this.size = size_;
+}
+
 var express = require('express');
 var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var server = app.listen(process.env.PORT || 3000, listen);
+app.use(express.static('public'));
+app.use(express.static('public'));
+var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/public'));
+function listen() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Listening at http://' + host + ':' + port);
+}
+
+// PLAYERS IN GAME
+var survivors = [];
 
 io.on('connection', function (socket) {
-    // Gets random ID for connected user
-    var user = Math.floor(Math.random() * 90000) + 10000;
+    // HANDLES NEW PLAYERS GETTING CONNECTED
+    socket.on('new player', (data) => {
+        console.log("New player!", socket.id);
+        var survivor = new Survivor(socket.id, data.name, data.loc.x, data.loc.y, data.size)
+        survivors.push(survivor);
+    });
 
-    //Adds user ID to list and prints it
-    console.log("connected", socket.id);
-    
+    // HANDLES ALL THE PLAYER MOUVEMENTS AND UPDATES
+    socket.on('update', function (data) {
+        survivors.forEach(survivor => {
+            if (socket.id == survivor.id) {
+                survivor.x = data.loc.x;
+                survivor.y = data.loc.y;
+                survivor.size = data.size;
+            }
+        });
+    });
 
-    
-
-    // Disconnects player when the leave the web page
+    // HANDLES THE PLAYER DISCONNECT
     socket.on('disconnect', function () {
-        // Removes disconnected user from list
-        console.log("disconnected");
+        console.log('Client has disconnected');
+        survivors.pop(socket.id)
     });
 });
 
-http.listen(8080, function () {
-    console.log('Connect to localhost');
-});
+//SENDS PLAYER LIST TO CLIENTS
+setInterval(() => {
+    io.sockets.emit('state', survivors);
+}, 1000 / 60);
