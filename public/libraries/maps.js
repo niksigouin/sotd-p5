a3fab7f1e1a2d0a83fbbfbd9794eb63de9cbc530
Nikolas Sigouin <nik.sigouin@gmail.com>
2020-05-02 04:15:13 -0400

    Map Rework

--- public/libraries/maps.js
+++ public/libraries/maps.js
@@ -1,167 +1,167 @@
 function map0() {
 
-    this.debug = true;
-  
-    this.waitingZone = {
-      x: 0,
-      y: 0,
-      w: 200,
-      h: height
-    }
-  
-    this.playArea = {
-      x: this.waitingZone.w,
-      y: 0,
-      w: width - 200,
-      h: height
-    }
-  
-    this.spawns = [
-      {
-        id: 0,
-        x: this.playArea.x + (this.playArea.w / 2)-425,
-        y: this.playArea.y + (this.playArea.h / 2)-275
-      },
-      {
-        id: 1,
-        x: this.playArea.x + (this.playArea.w / 2)-225,
-        y: this.playArea.y + (this.playArea.h / 2)-275
-      },
-      {
-        id: 2,
-        x: this.playArea.x + (this.playArea.w / 2)+0,
-        y: this.playArea.y + (this.playArea.h / 2)-275
-      },
-      {
-        id: 3,
-        x: this.playArea.x + (this.playArea.w / 2)+200,
-        y: this.playArea.y + (this.playArea.h / 2)-275
-      },
-      {
-        id: 4,
-        x: this.playArea.x + (this.playArea.w / 2)+400,
-        y: this.playArea.y + (this.playArea.h / 2)-275
-      },
-      {
-        id: 5,
-        x: this.playArea.x + (this.playArea.w / 2)+400,
-        y: this.playArea.y + (this.playArea.h / 2)-75
-      },
-      {
-        id: 6,
-        x: this.playArea.x + (this.playArea.w / 2)+400,
-        y: this.playArea.y + (this.playArea.h / 2)+125
-      },
-      {
-        id: 7,
-        x: this.playArea.x + (this.playArea.w / 2)+400,
-        y: this.playArea.y + (this.playArea.h / 2)+275
-      },
-  
-    ]
-  
-    this.obstacles = [
-      {
-        id: 0,
-        x: this.playArea.x + (this.playArea.w / 2) - 325,
-        y: this.playArea.y + (this.playArea.h / 2) - 200,
-        w: 200,
-        h: 60
-      },
-      {
-        id: 1,
-        x: this.playArea.x + (this.playArea.w / 2) + 0,
-        y: this.playArea.y + (this.playArea.h / 2)-200,
-        w: 200,
-        h: 60
-      },
-      {
-        id: 2,
-        x: this.playArea.x + (this.playArea.w / 2)+ 325,
-        y: this.playArea.y + (this.playArea.h / 2)-200,
-        w: 200,
-        h: 60
-      },
-      {
-        id: 3,
-        x: this.playArea.x + (this.playArea.w / 2)-325,
-        y: this.playArea.y + (this.playArea.h / 2)+50,
-        w: 60,
-        h: 200
-      },
-      {
-        id: 4,
-        x: this.playArea.x + (this.playArea.w / 2) + 0,
-        y: this.playArea.y + (this.playArea.h / 2) + 50,
-        w: 200,
-        h: 60
-      },
-      {
-        id: 5,
-        x: this.playArea.x + (this.playArea.w / 2) + 325,
-        y: this.playArea.y + (this.playArea.h / 2) + 50,
-        w: 60,
-        h: 200
-      }
-    ];
-  
-  
-    this.display = () => {
-      // WAITING ZONE
-      push();
-      fill("#cb4154");
-      translate(this.waitingZone.x, this.waitingZone.y)
-      rect(0, 0, this.waitingZone.w, this.waitingZone.h);
-      pop();
-  
-      // PLAY AREA 
-      push();
-      fill('#F2F2F2');
-      rect(this.playArea.x, this.playArea.y, this.playArea.w, this.playArea.h);
-      pop();
-  
-  
-      
-      push();
-      // display spawns
-      translate(this.playArea.x + (this.playArea.w / 2), this.playArea.y + (this.playArea.h / 2));
-      for (let i = 0; i < this.spawns.length; i++) {
-        if (this.debug) {
-          fill('#00BB00')
-          circle(this.spawns[i].x, this.spawns[i].y, 30);
-          fill(0);
-  
-  
-          textSize(20);
-          textAlign(CENTER, CENTER);
-          text(this.spawns[i].id, this.spawns[i].x, this.spawns[i].y)
-        }
-  
-      }
-      pop();
-      
-      push();
-      // display obstacles
-      for (let i = 0; i < this.obstacles.length; i++) {
-        push()
-        fill(0, 0, 255, 100)
-        rectMode(CENTER);
-        rect(this.obstacles[i].x, this.obstacles[i].y, this.obstacles[i].w, this.obstacles[i].h);
-  
-        if (this.debug) {
-          fill(0);
-          textSize(20);
-          textAlign(CENTER, CENTER);
-          text(this.obstacles[i].id, this.obstacles[i].x, this.obstacles[i].y)
-        }
-  
-        pop()
-      }
-      pop();
-    }
+  this.debug = true;
 
-    this.getSpawn = () => {
-        var randomSpawn = this.spawns[int(random(0, this.spawns.length))];
-        return randomSpawn;
-    }
+  this.waitingZone = {
+    x: 0,
+    y: 0,
+    w: 200,
+    h: height
   }
+
+  this.playArea = {
+    x: this.waitingZone.w,
+    y: 0,
+    w: width - this.waitingZone.w,
+    h: height
+  }
+
+  this.spawns = [
+    {
+      id: 0,
+      x: - 325,
+      y: - 295,
+    },
+    {
+      id: 1,
+      x: - 170,
+      y: - 295
+    },
+    {
+      id: 2,
+      x: - 0,
+      y: - 295
+    },
+    {
+      id: 3,
+      x: + 170,
+      y: - 295
+    },
+    {
+      id: 4,
+      x: + 325,
+      y: - 295
+    },
+    {
+      id: 5,
+      x: + 200,
+      y: - 75
+    },
+    {
+      id: 6,
+      x: + 200,
+      y: + 125
+    },
+    {
+      id: 7,
+      x: + 200,
+      y: + 275
+    },
+
+  ]
+
+  this.obstacles = [
+    {
+      id: 0,
+      x: this.playArea.x + (this.playArea.w / 2) - 325,
+      y: this.playArea.y + (this.playArea.h / 2) - 200,
+      w: 200,
+      h: 60
+    },
+    {
+      id: 1,
+      x: this.playArea.x + (this.playArea.w / 2) - 0,
+      y: this.playArea.y + (this.playArea.h / 2) - 200,
+      w: 200,
+      h: 60
+    },
+    {
+      id: 2,
+      x: this.playArea.x + (this.playArea.w / 2) + 325,
+      y: this.playArea.y + (this.playArea.h / 2) - 200,
+      w: 200,
+      h: 60
+    },
+    {
+      id: 3,
+      x: this.playArea.x + (this.playArea.w / 2) - 325,
+      y: this.playArea.y + (this.playArea.h / 2) + 50,
+      w: 60,
+      h: 200
+    },
+    {
+      id: 4,
+      x: this.playArea.x + (this.playArea.w / 2) - 0,
+      y: this.playArea.y + (this.playArea.h / 2) + 50,
+      w: 200,
+      h: 60
+    },
+    {
+      id: 5,
+      x: this.playArea.x + (this.playArea.w / 2) + 325,
+      y: this.playArea.y + (this.playArea.h / 2) + 50,
+      w: 60,
+      h: 200
+    }
+  ];
+
+
+  this.display = () => {
+    // WAITING ZONE
+    push();
+    fill("#cb4154");
+    translate(this.waitingZone.x, this.waitingZone.y)
+    rect(0, 0, this.waitingZone.w, this.waitingZone.h);
+    pop();
+
+    // PLAY AREA 
+    push();
+    fill('#F2F2F2');
+    rect(this.playArea.x, this.playArea.y, this.playArea.w, this.playArea.h);
+    pop();
+
+
+
+    push();
+    // display spawns
+    translate(this.playArea.x + (this.playArea.w / 2), this.playArea.y + (this.playArea.h / 2));
+    for (let i = 0; i < this.spawns.length; i++) {
+      if (this.debug) {
+        fill('#00BB00')
+        circle(this.spawns[i].x, this.spawns[i].y, 30);
+        fill(0);
+
+
+        textSize(20);
+        textAlign(CENTER, CENTER);
+        text(this.spawns[i].id, this.spawns[i].x, this.spawns[i].y)
+      }
+
+    }
+    pop();
+
+    push();
+    // display obstacles
+    for (let i = 0; i < this.obstacles.length; i++) {
+      push()
+      fill(0, 0, 255, 100)
+      rectMode(CENTER);
+      rect(this.obstacles[i].x, this.obstacles[i].y, this.obstacles[i].w, this.obstacles[i].h);
+
+      if (this.debug) {
+        fill(0);
+        textSize(20);
+        textAlign(CENTER, CENTER);
+        text(this.obstacles[i].id, this.obstacles[i].x, this.obstacles[i].y)
+      }
+
+      pop()
+    }
+    pop();
+  }
+
+  this.getSpawn = () => {
+    var randomSpawn = this.spawns[int(random(0, this.spawns.length))];
+    return randomSpawn;
+  }
+}
